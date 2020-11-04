df8df9d4514216c595b504ea3f42d5513f3398b9
juango8 <jgonzalofh2898@gmail.com>
2020-11-04 09:36:21 -0500

    Android and Ios Project

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import '@codetrix-studio/capacitor-google-auth';
+import { Plugins } from '@capacitor/core';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  userInfo = null;
 
   constructor() {}
 
+  async googleSignup() {
+    const googleUser = await Plugins.GoogleAuth.signIn(null);
+    console.log('user: ', googleUser);
+    this.userInfo = googleUser;
+  }
 }
